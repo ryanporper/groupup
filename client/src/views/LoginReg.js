7d430605c18fcfcc2ab93b76e3b0df88805e43e1
ryanporper <ryan_porper@aol.com>
2022-09-19 17:15:49 -0400

    added home button

--- client/src/views/LoginReg.js
+++ client/src/views/LoginReg.js
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom";
 
-export const LoginReg = (props) => {  
+export const LoginReg = () => {  
 
   return (
-    <div><h1>Coming soon...</h1></div>
+    <div>
+      <h1>Coming soon...</h1>
+      <Link to="/groups" className="btn btn-outline-primary mx-1">
+            Home
+      </Link>
+      </div>
+    
   );
 };
 
